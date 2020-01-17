1e5da33af8f4a9c235895d8f6ae9ea615a422e77
Umberto Valente <uvalente89@gmail.com>
2020-01-17 10:31:29 +0000

    Created update bookmark test

--- spec/feature/update_bookmark_spec.rb
+++ spec/feature/update_bookmark_spec.rb
@@ -6,6 +6,11 @@ feature 'add bookmark' do
     fill_in 'url', with: 'http://www.mock.com'
     fill_in 'title', with: 'Mock'
     click_button 'Add Bookmark'
-    expect(page).to have_link('Mock', href: 'http://www.mock.com')
+    within(:css, "form.update") do
+      fill_in 'url', with: 'http://www.newurl.com'
+      fill_in 'title', with: 'New Mock'
+      click_button 'Update Mock'
+    end
+    expect(page).to have_link('New Mock', href: 'http://www.newurl.com')
   end
 end
